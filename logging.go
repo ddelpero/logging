5b3524b5a6868fa4074ceb3498d9bcf752fa66b2
Derek D <ddelpero@gmail.com>
2023-10-02 14:07:35 -0500

    Added zipping old file

--- logging.go
+++ logging.go
@@ -20,39 +20,82 @@ func SetLogFileName(fileName string) {
 //simple rotation based on day changing
 func rotate() {
 	if time.Now().Day() != logDate.Day() {
-		newLogFileName := LogFileName + "." + time.Now().Format("2006-02-01")
+		newLogFileName = fmt.Sprintf("%s.%s.log", strings.Replace(LogFileName, ".log", "", 1), time.Now().Format("2006-01-02"))
 		logDate = time.Now()
 		os.Rename(LogFileName, newLogFileName)
 	}
 }
 
+func ZipLog(newLogFileName string) {
+	//archive log file
+	archiveLogFileName := LogFileName + "." + time.Now().Format("2006-02-01")
+	archive, err := os.Create(archiveLogFileName + ".zip")
+	if err != nil {
+		log.Errorln("Error creating archive log file: ", err)
+	}
+	defer archive.Close()
+
+	zipWriter := zip.NewWriter(archive)
+	defer zipWriter.Close()
+	f, err := os.Open(newLogFileName)
+	if err != nil {
+		log.Errorln("Error opening log file: ", err)
+	}
+
+	_, filename := filepath.Split(newLogFileName)
+	zipW, err := zipWriter.Create(filename)
+	if err != nil {
+		log.Errorln("Error adding zip file: ", err)
+	}
+	if _, err = io.Copy(zipW, f); err != nil {
+		log.Errorln("Error copying file to zip: ", err)
+	}
+	f.Close()
+	zipWriter.Close()
+	os.Remove(newLogFileName)
+	// don't log the error; otherwise, we'll get an infinite loop
+	// if err != nil {
+	// 	log.Errorln("Error removing log file: ", err)
+	// }
+}
+
 func Debugln(msg ...interface{}) {
-	rotate()
+	newLogFileName := rotate()
 	log.Debugln(msg...)
+	if newLogFileName != "" {
+		ZipLog(newLogFileName)
+	}
 }
 
 func Errorln(msg ...interface{}) {
-	rotate()
+	newLogFileName := rotate()
 	log.Errorln(msg...)
+	if newLogFileName != "" {
+		ZipLog(newLogFileName)
+	}
 }
 
 func Errorf(msg ...interface{}) {
-	rotate()
+	newLogFileName := rotate()
 	log.Errorf(msg...)
+	if newLogFileName != "" {
+		ZipLog(newLogFileName)
+	}
 }
 
 func Fatal(msg ...interface{}) {
-	rotate()
+	newLogFileName := rotate()
 	log.Fatal(msg...)
+	if newLogFileName != "" {
+		ZipLog(newLogFileName)
+	}
 }
 
 func Println(msg ...interface{}) {
-	rotate()
+	newLogFileName := rotate()
 	log.Println(msg...)
+	if newLogFileName != "" {
+		ZipLog(newLogFileName)
+	}
 }
-
-
-
-
-
 
